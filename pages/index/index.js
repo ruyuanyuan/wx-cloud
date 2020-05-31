5f311ac7961d6f504cd16024095536ea055dc65c
ruyuanyuan <947532851@qq.com>
2020-05-31 21:15:04 +0800

    文件存储

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,4 +1,7 @@
 Page({
+  data:{
+    imgUrl:""
+  },
   sumNumber(){
     wx.cloud.callFunction({
       name: 'add',
@@ -26,5 +29,36 @@ Page({
         }
       }
     )
+  },
+  uploadEvent(){
+    wx.chooseImage({
+      success: chooseResult => {
+        console.log(chooseResult)
+        // 将图片上传至云存储空间
+        wx.cloud.uploadFile({
+          // 指定上传到的云路径
+          cloudPath: new Date().getTime()+'.png',
+          // 指定要上传的文件的小程序临时文件路径
+          filePath: chooseResult.tempFilePaths[0],
+          // 成功回调
+          success: res => {
+            console.log('上传成功', res)
+            this.setData({
+              imgUrl:res.fileID
+            })
+          },
+        })
+      },
+    })
+  },
+  uploadvideo(){
+    wx.chooseVideo({
+      sourceType: ['album','camera'],
+      maxDuration: 60,
+      camera: 'back',
+      success(res) {
+        console.log(res.tempFilePath)
+      }
+    })
   }
 })
