800afa2cbe77e5e9669e7743397df43edf563243
ruyuanyuan <947532851@qq.com>
2020-05-31 16:07:21 +0800

    数据库学习

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,75 @@
-//index.js
-//获取应用实例
-const app = getApp()
 
+const db = wx.cloud.database().collection('list')
+let name=''
+let age = ''
+let delId = ''
+let updateId = ''
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  addName(event){
+    console.log(event.detail.value)
+    name = event.detail.value
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
+  addAge(event){
+    console.log(event.detail.value)
+    age = event.detail.value
+  },
+  delDataId(event){
+    console.log(event.detail.value)
+    delId = event.detail.value
+  },
+  updateDataId(event){
+    updateId = event.detail.value
+  },
+  // 添加数据
+  addData(){
+    db.add({
+      data:{
+        name:name,
+        age:age
+      },
+      success(res){
+        console.log('添加成功：',res)
+      },
+      fail(err){
+        console.log('添加失败：',err)
+      }
     })
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
+  //查询数据
+  getData(){
+    db.get({
+      success(res){
+        console.log('查询成功：',res)
+      },
+      fail(err){
+        console.log('查询失败：',err)
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+    })
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+  //删除数据
+  delData(){
+    console.log(delId)
+    db.doc(delId).remove({
+      success(res) {
+        console.log('删除数据成功',res)
+      },
+      fail(err){
+        console.log('删除数据失败',err)
+      }
+    })
+  },
+  //update数据
+  updateData(){
+    db.doc(updateId).update({
+      data:{
+        name:name
+      },
+      success(res) {
+        console.log('更新数据成功',res)
+      },
+      fail(err){
+        console.log('更新数据失败',err)
+      }
     })
   }
 })
